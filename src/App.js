ecdca9feada8625d539254b9e9ec42a5e8077e90
Lucas Setiady <luca.setia@gmail.com>
2025-01-25 14:18:26 +1000

    Implemented Visual Aspects for the Title Area

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <div className="Title">Title Card</div>
-    <div className="IconsSignIn">Icons & Sign In</div>
-    <div className="SearchBar">Search Bar</div>
+    <div className="Title"><h2>Plan Finder</h2></div>
+    <div className="IconsSignIn">
+      <button className="buttons">Sign In</button>
+      <button className="buttons">Register</button>
+    </div>
+    <div >
+      <h1 className="InfoCard1">Search for things to do on your holiday!</h1>
+      <p className="InfoCard2">Look for attractions, parks, museums and more... </p>
+    </div>
+    <div className="SearchBar">
+      <input type="search" className="SearchArea" placeholder="Search for Plan..."></input>
+    </div>
     <div className="sortButton">Sort Button</div>
     <div className="filterButtons">Filters Section</div>
     <div className="searchResults">Search Results - This section will need to update to allow scrolling to the bottom tho</div>
