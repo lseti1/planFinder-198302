1abeb0ff1f72897fda5e7745520b5e88ef4b4983
Lucas Setiady <luca.setia@gmail.com>
2025-01-27 15:26:38 +1000

    Update Search Bar with Console Logging

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [userSearch, setUserSearch] = useState("");
+
+  const handleInput = (event) => {
+    const value = event.target.value;
+    setUserSearch(value);
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault(); // This stops the page from reloading
+    console.log("Searched Plan: ", userSearch);
+  };
+
   return (
+    <form onSubmit={handleSearch}>
     <div className="App">
     <div className="Title"><h2>Plan Finder</h2></div>
     <div className="IconsSignIn">
@@ -13,12 +27,13 @@ function App() {
       <p className="InfoCard2">Look for attractions, parks, museums and more... </p>
     </div>
     <div className="SearchBar">
-      <input type="search" className="SearchArea" placeholder="Search for Plan..."></input>
+      <input type="search" className="SearchArea" placeholder="Search for Plan..." onChange = {handleInput} value = {userSearch}></input>
     </div>
     <div className="sortButton">Sort By:</div>
     <div className="filterButtons">Filters Section</div>
     <div className="searchResults">Search Results - This section will need to update to allow scrolling to the bottom tho</div>
     </div>
+    </form>
   );
 }
 
