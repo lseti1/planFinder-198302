820c24581017427b915244dda605537ee6f2b758
Lucas Setiady <luca.setia@gmail.com>
2025-04-14 11:47:58 +1000

    Style/Bug: Fixed styling for Modal pop up on potrait and landscape phone screens

--- src/components/modals/forms/SignInForm.js
+++ src/components/modals/forms/SignInForm.js
@@ -6,15 +6,11 @@ const SignInForm = () => {
             <h1>Log In</h1>
             <input className='modalTextInputs' type='text' placeholder='Email Address' />
             <input className='modalTextInputs' type='text' placeholder='Password' />
-            <button className="linkedButton">
-              Forgot Password?
-            </button>
+            <button className="linkedButton">Forgot Password?</button>
             <button className='modalSubmitButton' type='submit'>Sign In</button>
             <button className='linkedButton'>Can't Access Account?</button>
             <p>Don't have an account?
-              <button className="linkedButton">
-                Sign Up Here
-              </button>
+              <button className="linkedButton">Sign Up Here</button>
             </p>
             <p className='disclaimer'>Please Note: For visual purposes, Sign In capabilities are off.</p>
         </>
